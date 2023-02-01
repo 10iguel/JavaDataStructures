a4cbaa5a44754106462a9e33f695b08f35d49b8e
Miguel <miguel,espinoza.silloca@gmail.com>
2023-01-31 23:23:09 -0700

    queue

--- src/edu/ensign/cs235/queue/QueueOnLinkedList.java
+++ src/edu/ensign/cs235/queue/QueueOnLinkedList.java
@@ -1,29 +1,82 @@
 package edu.ensign.cs235.queue;
 
+class NodeList{
+    int value;
+    NodeList next;
+    public NodeList(int value, NodeList next){
+        this.value = value;
+        this.next = next;
+    }
+}
+
 public class QueueOnLinkedList {
+
+    private NodeList head;
+
     public boolean is_empty() {
-        return true;
+        return head == null;
     }
 
     public void enqueue(int value) {
+        if (head == null){
+            head = new NodeList(value, head);
+        }else {
+            NodeList current = head;
+            while (current.next != null){
+                current = current.next;
+            }
+            current.next = new NodeList(value, null);
+        }
     }
 
     public int get_size() {
-        return 0;
+        if (head == null){
+            return 0;
+        }
+        NodeList current = head;
+        int count = 0;
+        while (current.next !=null){
+            count++;
+            current = current.next;
+        }
+        return count+1;
     }
 
     public int peek_front() {
-        return 0;
+        return head.value;
     }
 
     public int dequeue() {
-        return 0;
+        int value = head.value;
+        head = head.next;
+        return value;
     }
 
     public int peek_rear() {
-        return 0;
+        NodeList current = head;
+        while (current.next != null){
+            current = current.next;
+        }
+        return  current.value;
     }
 
     public void clear() {
+        head = null;
+    }
+
+    @Override
+    public String toString() {
+        if (is_empty()){
+            return "[]";
+        }else {
+            String text = "[";
+            NodeList current = head;
+            while (current.next != null){
+                text = text + current.value + ", ";
+                current = current.next;
+            }
+            text = text + current.value + "]";
+            return text;
+        }
     }
 }
